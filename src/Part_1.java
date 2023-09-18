f5db39773a76d8a0787b51671171781f65afc970
DimaK128 <dmytro.kolesnyk@auk.edu.ua>
2023-09-19 00:04:34 +0300

    1

--- src/Part_1.java
+++ src/Part_1.java
@@ -1,2 +1,91 @@
-package PACKAGE_NAME;public class Part_1 {
+import java.util.Random;
+import java.util.Scanner;
+
+public class Part_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            int[] arr;
+            int n;
+
+            System.out.println("Enter the number of elements: ");
+            n = scanner.nextInt();
+
+            System.out.println("Choose an option (1 for random values, 2 for user input): ");
+            int choice = scanner.nextInt();
+
+            if (choice == 1) {
+                arr = generateRandomArray(n);
+            } else if (choice == 2) {
+                arr = getUserInputArray(n);
+            } else {
+                System.out.println("Invalid choice. Please enter 1 or 2.");
+                continue;
+            }
+
+            System.out.println("Original array:");
+            printArray(arr);
+
+            bubbleSort(arr);
+
+            System.out.println("Sorted array:");
+            printArray(arr);
+
+            System.out.println("Do you want to repeat the process? (1 for Yes and 0 for No): ");
+        } while (scanner.nextInt() == 1);
+
+        System.out.println("Have a nice day! :D!");
+        scanner.close();
+    }
+
+    // Method to generate an array of random values
+    private static int[] generateRandomArray(int n) {
+        int[] arr = new int[n];
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            arr[i] = random.nextInt(100); // Adjust the range as needed
+        }
+        return arr;
+    }
+
+    // Method to create an array with values from user input
+    private static int[] getUserInputArray(int n) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        return arr;
+    }
+
+    // Method to perform the bubble sort algorithm
+    private static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break; // If no two elements were swapped in inner loop, the array is already sorted
+            }
+        }
+    }
+
+    // Method to print an array
+    private static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
 }
--- src/Part_2.java
+++ src/Part_2.java
@@ -1,2 +1,72 @@
-package PACKAGE_NAME;public class Part_2 {
+import java.util.Arrays;
+import java.util.Random;
+
+public class Part_2 {
+    public static void main(String[] args) {
+        // Generate a random array with 6 elements
+        int[] array = generateRandomArray(6);
+
+        // Display the initial array
+        System.out.println("Initial array:");
+        printArray(array);
+
+        // Perform bubble sort and create iteration snapshots
+        bubbleSortWithSnapshots(array);
+    }
+
+    // Function to generate a random array with n elements
+    private static int[] generateRandomArray(int n) {
+        Random rand = new Random();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = rand.nextInt(100); // Random integers between 0 and 99
+        }
+        return array;
+    }
+
+    // Function to perform bubble sort and create iteration snapshots
+    private static void bubbleSortWithSnapshots(int[] array) {
+        int n = array.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            System.out.println("\nOuter loop, iteration " + (i + 1) + ":");
+            printArray(array);
+
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.println("\nInner loop:");
+                printArray(array);
+                System.out.println("Comparing elements at indices " + j + " and " + (j + 1));
+
+                if (array[j] > array[j + 1]) {
+                    // Swap the elements
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+
+                    System.out.println("Swapping elements at indices " + j + " and " + (j + 1) + ":");
+                    printArray(array);
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                // If no swaps were made in this pass, the array is already sorted
+                break;
+            }
+        }
+
+        System.out.println("\nFinal sorted array:");
+        printArray(array);
+    }
+
+    // Function to print an array
+    private static void printArray(int[] array) {
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
 }
