071c0d61319ac62b174b0cd43e576de8feeee48b
DimaK128 <dmytro.kolesnyk@auk.edu.ua>
2023-09-26 13:08:10 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -47,19 +47,14 @@ public class Main {
 
     public static void bubbleSort(int[] arr) {
         int n = arr.length;
-        boolean swapped;
         for (int i = 0; i < n - 1; i++) {
-            swapped = false;
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];                                    // Swap arr[j] and arr[j+1]
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
-                    swapped = true;
+
                 }
-            }
-            if (!swapped) {
-                break; // If no two elements were swapped in inner loop, the array is already sorted and it stops
             }
         }
     }
