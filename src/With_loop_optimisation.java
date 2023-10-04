491250c73b1b391d67edf14be8d749589dbd6be1
DimaK128 <dmytro.kolesnyk@auk.edu.ua>
2023-10-04 21:04:36 +0300

    1

--- src/With_loop_optimisation.java
+++ src/With_loop_optimisation.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class Main {
+public class With_loop_optimisation {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -10,24 +10,12 @@ public class Main {
             System.out.println("Enter the number of elements: ");
             int n = scanner.nextInt();
 
-            int[] arr;
-
-            System.out.println("Do you want to generate random values or input them by yourself? (Random/Input): ");
-            String choice = scanner.next();
-
-            if (choice.equalsIgnoreCase("Random")) {
-                arr = generateRandomArray(n);
-            } else if (choice.equalsIgnoreCase("Input")) {
-                arr = getInputArray(n, scanner);
-            } else {
-                System.out.println("Invalid choice. Please enter 'generate' or 'input'.");
-                continue;
-            }
+            int[] arr = generateRandomArray(n);
 
             System.out.println("Original array:");
             printArray(arr);
 
-            bubbleSort(arr);
+            bubbleSortWithOptimization(arr);
 
             System.out.println("Sorted array:");
             printArray(arr);
@@ -45,25 +33,39 @@ public class Main {
         scanner.close();
     }
 
-    public static void bubbleSort(int[] arr) {
+    public static void bubbleSortWithOptimization(int[] arr) {
+        // Outer loop
         int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
+        boolean swapped = true;
+
+        for (int i = 0; i < n - 1 && swapped; i++) {
+            swapped = false;
+// The outer loop optimization works by initializing a boolean variable swapped to true before the outer loop.
+// If no swaps are made in any iteration of the outer loop, then the array is already sorted and we can break out of the loop.
+// This is because if no swaps are needed in any iteration of the outer loop, then the array must already be sorted.
+
+
+            // Inner loop
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];                                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
 
+                    swapped = true;
                 }
             }
         }
     }
+// The inner loop optimization works by checking if a swap was made at the end of each iteration of the inner loop.
+// If no swap was made, then the array is already sorted and we can break out of the inner loop.
+// This is because if no swaps are needed, then the elements in the array are already in the correct order.
 
     public static int[] generateRandomArray(int n) {
         int[] arr = new int[n];
         Random rand = new Random();
         for (int i = 0; i < n; i++) {
-            arr[i] = rand.nextInt(100);      // Generates random values from 1 to 100
+            arr[i] = rand.nextInt(100);
         }
         return arr;
     }
@@ -76,12 +78,18 @@ public class Main {
             System.out.print("Element #" + (i + 1) + ": ");
             arr[i] = scanner.nextInt();
         }
-        return arr;                           // Here user types elements of his array and that array returns back
+        return arr;
     }
 
     public static void printArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            System.out.println("Element #" + (i + 1) + ": " + arr[i]);   // Prints elements of our array
+            System.out.println("Element #" + (i + 1) + ": " + arr[i]);
         }
     }
 }
+
+
+// In general, the code with loop optimization avoids unnecessary iterations of the outer and inner loops.
+
+
+
---
+++ src/Without_any_loop.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Without_any_loop {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.println("Enter the number of elements: ");
+            int n = scanner.nextInt();
+
+            int[] arr;
+
+            System.out.println("Do you want to generate random values or input them by yourself? (Random/Input): ");
+            String choice = scanner.next();
+
+            if (choice.equalsIgnoreCase("Random")) {
+                arr = generateRandomArray(n);
+            } else if (choice.equalsIgnoreCase("Input")) {
+                arr = getInputArray(n, scanner);
+            } else {
+                System.out.println("Invalid choice. Please enter 'generate' or 'input'.");
+                continue;
+            }
+
+            System.out.println("Original array:");
+            printArray(arr);
+
+            bubbleSort(arr);
+
+            System.out.println("Sorted array:");
+            printArray(arr);
+
+            System.out.println("Do you want to repeat? (Yes/No): ");
+            String repeat = scanner.next();
+
+            if (!repeat.equalsIgnoreCase("yes")) {
+                break;
+            }
+
+        } while (true);
+
+        System.out.println("Goodbye!");
+        scanner.close();
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            // Outer loop: Iterates over the array until no more swaps will be made.
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+    // Inner loop : Compares nearby elements and swaps them if they are in the wrong order.
+
+
+    public static int[] generateRandomArray(int n) {
+        int[] arr = new int[n];
+        Random rand = new Random();
+        for (int i = 0; i < n; i++) {
+            arr[i] = rand.nextInt(100);
+        }
+        return arr;
+    }
+    // Generates random values from 1 to 100
+
+    public static int[] getInputArray(int n, Scanner scanner) {
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " elements:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Element #" + (i + 1) + ": ");
+            arr[i] = scanner.nextInt();
+        }
+        return arr;
+    }
+    // Here program asks user to input numbers based on length of array
+    // Method returns the filled array with the numbers
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Element #" + (i + 1) + ": " + arr[i]);
+            // Prints elements of array
+        }
+    }
+}
+
